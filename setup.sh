323d4236bd124682b3908b5999dcd0b79dcde2bd
Kevin Zhang <kevzhang2022@gmail.com>
2025-09-17 11:29:04 -0500

    Update tmux config

--- setup.sh
+++ setup.sh
@@ -18,20 +18,21 @@ for cfg in ${CONFIGS[@]}; do
 	echo "Set symlink for $cfg"
 done
 
-# NeoVim dependencies 
-brew install nvm lazygit tmux
-# npm needed to install some LSPs
-nvm install node
+# Tmux
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Oh-My-ZSH plugins/themes
+# Oh-My-ZSH
 brew install autojump eza
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-# Setup NeoVim
+# NeoVim
 brew install neovim
 git clone git@github.com:N1v3x2/nvim.git ~/.config/nvim
+brew install nvm lazygit
+nvm install node # needed to install some LSPs
 
-# Setup Kitty
+# Kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 rm -rf ~/.config/kitty
 git clone git@github.com:N1v3x2/kitty.git ~/.config/kitty
