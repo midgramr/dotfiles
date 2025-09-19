811d2ee72f3abb6d7682a3d5ff3d972a654b7456
Kevin Zhang <kevzhang2022@gmail.com>
2025-09-19 17:30:35 -0500

    Update setup

--- setup.sh
+++ setup.sh
@@ -20,17 +20,19 @@ done
 
 # Tmux
 brew install tmux
+rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Oh-My-ZSH
 brew install autojump eza
+rm -rf $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # NeoVim
-brew install neovim
-git clone git@github.com:N1v3x2/nvim.git ~/.config/nvim
-brew install nvm lazygit
+brew install neovim nvm lazygit
 nvm install node # needed to install some LSPs
+rm -rf ~/.config/nvim
+git clone git@github.com:N1v3x2/nvim.git ~/.config/nvim
 
 # Kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
