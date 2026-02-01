fbbc0f693269ed312696b2eeeaad320b7ca433d3
Kevin Zhang <kevzhang2022@gmail.com>
2026-01-31 19:28:34 -0600

    Remove tmux plugin dir + add bin package

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-cat 'utils.txt' | xargs brew install
-cat 'packages.txt' | xargs stow --adopt --dotfiles -R -t ~ -v
+cat utils.txt | xargs brew install
+cat packages.txt | xargs stow --adopt --dotfiles -R -t ~ -v
 
 # Git
 git config --global core.pager delta
@@ -12,5 +12,8 @@ git config --global delta.navigate true
 
 # Need node to install some LSPs
 nvm install node 
+
+# Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # vim: ft=bash
