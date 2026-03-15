ec719207840ece1cff58268cdb7026e588eba95c
Kevin Zhang <kevzhang2022@gmail.com>
2026-03-15 12:01:16 -0500

    Update nvim statusline

--- nvim/dot-config/nvim/lua/plugins/mini.lua
+++ nvim/dot-config/nvim/lua/plugins/mini.lua
@@ -23,7 +23,7 @@ return { -- Collection of various small independent plugins/modules
       for i = 1, #clients do
         local name = clients[i]['name']
         if not seen[name] then
-          names = names .. ', ' .. clients[i]['name']
+          names = names .. ', ' .. name
           seen[name] = true
         end
       end
@@ -35,8 +35,8 @@ return { -- Collection of various small independent plugins/modules
       content = {
         active = function()
           local mode, mode_hl = statusline.section_mode { trunc_width = 100 }
-          local filename = statusline.section_filename { trunc_width = 140 }
-          local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+          local filename = statusline.section_filename { trunc_width = 999 }
+          local fileinfo = statusline.section_fileinfo { trunc_width = 999 }
           return statusline.combine_groups {
             { hl = mode_hl, strings = { mode } },
             '%<', -- Mark general truncate point
@@ -44,7 +44,7 @@ return { -- Collection of various small independent plugins/modules
             '%=', -- End left alignment
             { strings = { '%{v:lua.Get_lsp_client()}' } },
             { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-            { hl = mode_hl, strings = { '%2l/%-2L' } },
+            { hl = mode_hl, strings = { '%2l:%-2c' } },
           }
         end,
       },
