227ec6f26fae1c5deb8bb104b686b4c90f408397
Kevin Zhang <kevzhang2022@gmail.com>
2026-04-22 23:03:14 -0500

    Clean up zsh setup

--- zsh/dot-zsh/env.zsh
+++ zsh/dot-zsh/env.zsh
@@ -1,4 +1,5 @@
 export EDITOR=nvim
 export MANPAGER='nvim +Man! +"set so=99"'
 export JAVA_HOME=$(/usr/libexec/java_home 2>/dev/null)
+export LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 export PATH=$PATH:/Applications/kitty.app/Contents/MacOS
