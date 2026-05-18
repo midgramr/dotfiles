19ac6cfdf87f8d8844981b302459944672773f16
Kevin Zhang <kevzhang2022@gmail.com>
2026-05-17 19:30:39 -0500

    Update autopair functionality

--- nvim/.config/nvim/lua/config/autopair.lua
+++ nvim/.config/nvim/lua/config/autopair.lua
@@ -1,49 +1,87 @@
--- TODO: handle quotes and backticks
 local combos = {
   ['('] = ')',
   ['['] = ']',
   ['{'] = '}',
+  ["'"] = "'",
+  ['"'] = '"',
+  ['`'] = '`',
 }
 
-local ns = vim.api.nvim_create_namespace 'autopairs'
+-- If inserting in the middle of a line, only auto-insert bracket
+-- if the next character is a trigger character
+local trigger = {
+  [','] = true,
+  [' '] = true,
+  ['.'] = true,
+  [')'] = true,
+}
 
-local function insideautopair()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local row, col = cursor[1] - 1, cursor[2]
-  local extmarks = vim.api.nvim_buf_get_extmarks(0, ns, { row, col }, { row, col }, { limit = 1 })
+local function is_inside_pair()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
   local line = vim.api.nvim_get_current_line()
   local prev = line:sub(col, col)
   local next = line:sub(col + 1, col + 1)
-  return #extmarks > 0 and combos[prev] and combos[prev] == next
+  return combos[prev] and combos[prev] == next
+end
+
+local function is_inside_bracket()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  local line = vim.api.nvim_get_current_line()
+  local prev = line:sub(col, col)
+  local next = line:sub(col + 1, col + 1)
+  return prev ~= next and combos[prev] and combos[prev] == next
 end
 
 for left, right in pairs(combos) do
-  vim.keymap.set('i', left, function()
-    vim.api.nvim_put({ left .. right }, 'c', false, false)
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local row, col = cursor[1], cursor[2]
-    vim.api.nvim_buf_set_extmark(0, ns, row - 1, col, { invalidate = true })
-  end)
+  if left == right then
+    vim.keymap.set('i', left, function()
+      local col = vim.api.nvim_win_get_cursor(0)[2]
+      local line = vim.api.nvim_get_current_line()
+      local next = line:sub(col + 1, col + 1)
+      if next == right then
+        return '<Right>'
+      elseif col == #line then
+        if left == '`' and #line > 1 then
+          local prev1 = line:sub(col, col)
+          local prev2 = line:sub(col - 1, col - 1)
+          if prev1 == '`' and prev2 == '`' then
+            return string.rep('`', 4) .. string.rep('<Left>', 3)
+          end
+        end
+        return left .. right .. '<Left>'
+      else
+        return left
+      end
+    end, { expr = true })
+  else
+    vim.keymap.set('i', left, function()
+      local col = vim.api.nvim_win_get_cursor(0)[2]
+      local line = vim.api.nvim_get_current_line()
+      local next = line:sub(col + 1, col + 1)
+      if col == #line or trigger[next] then
+        return left .. right .. '<Left>'
+      else
+        return left
+      end
+    end, { expr = true })
 
-  vim.keymap.set('i', right, function()
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local row, col = cursor[1] - 1, cursor[2]
-    local extmarks = vim.api.nvim_buf_get_extmarks(0, ns, { row, col }, { row, col }, { limit = 1 })
-    local line = vim.api.nvim_get_current_line()
-    local next = line:sub(col + 1, col + 1)
-    if #extmarks > 0 and next == right then
-      -- TODO: handle case when blink.cmp autoinserts a pair during completion
-      return '<Right>'
-    else
-      return right
-    end
-  end, { expr = true })
+    vim.keymap.set('i', right, function()
+      local col = vim.api.nvim_win_get_cursor(0)[2]
+      local line = vim.api.nvim_get_current_line()
+      local next = line:sub(col + 1, col + 1)
+      if next == right then
+        return '<Right>'
+      else
+        return right
+      end
+    end, { expr = true })
+  end
 end
 
 vim.keymap.set('i', '<BS>', function()
-  return insideautopair() and '<BS><Del>' or '<BS>'
+  return is_inside_pair() and '<BS><Del>' or '<BS>'
 end, { expr = true })
 
 vim.keymap.set('i', '<CR>', function()
-  return insideautopair() and '<CR><Esc>O' or '<CR>'
+  return is_inside_bracket() and '<CR><Esc>O' or '<CR>'
 end, { expr = true })
