9fc0b0217c3b86b2f71bd3a75203723d5d033b96
Kevin Zhang <kevin.zhang@thetradedesk.com>
2026-07-26 17:40:27 -0600

    Update colors

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -4,7 +4,7 @@
 polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "---" | tee -a /tmp/polybar.log
+polybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bars launched..."
